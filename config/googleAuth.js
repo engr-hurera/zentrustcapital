86561ea767e233d244615ba48cf1edd347ae00cb
engr-hurera <engr.abuhurera@gmail.com>
2026-08-19 01:18:55 +0500

    update

--- config/googleAuth.js
+++ config/googleAuth.js
@@ -1,4 +1,22 @@
+// const { google } = require("googleapis");
+
+// const googleOAuth2Client = new google.auth.OAuth2(
+//   process.env.GOOGLE_CLIENT_ID,
+//   process.env.GOOGLE_CLIENT_SECRET,
+//   process.env.GOOGLE_CALLBACK_URL
+// );
+
+// module.exports = googleOAuth2Client;
+
+
 const { google } = require("googleapis");
+
+// TEMPORARY DEBUG LOGS (Remove after fixing)
+console.log("--- GOOGLE AUTH ENVIRONMENT CHECK ---");
+console.log("Callback URL being used:", process.env.GOOGLE_CALLBACK_URL);
+console.log("Client ID Length:", process.env.GOOGLE_CLIENT_ID ? process.env.GOOGLE_CLIENT_ID.length : "UNDEFINED");
+console.log("First 5 chars of Client ID:", process.env.GOOGLE_CLIENT_ID ? process.env.GOOGLE_CLIENT_ID.substring(0, 5) : "NONE");
+console.log("-------------------------------------");
 
 const googleOAuth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
